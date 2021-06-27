ea573631eaabf00b93ae8b338e4aae985edd39c4
mathvaillant <matheusvaillant505@gmail.com>
2021-06-27 17:39:50 +0100

    updates favicon

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,7 @@ window.addEventListener('DOMContentLoaded', (e) => {
     ? animation.classList.remove('hide')
     : setTimeout(() => {
         animation.classList.add('hide')
-      }, 3000)
+      }, 000)
 })
 /*===== MENU SHOW =====*/
 const showMenu = (toggleId, navId) => {
